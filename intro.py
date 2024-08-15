1e7667e826c61d3cb1222e7cf0ade9a8b12b8506
Focux45 <134445296+Focux45@users.noreply.github.com>
2024-08-15 07:28:18 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -7,3 +7,13 @@ image = Image.open("mclaren.jpg")
 st.image(image,caption = "mclaren")
 texto = st.text_input('Aviso parroquial',"Tienes 20 años y no has conseguido un carro deportivo, pues que lastima yo tampoco")
 st.write("El texto escrito es", texto)
+
+st.subheader("2 Columnas")
+col1, col2 = st.columns(2)
+
+with col1:
+  st.subheader("Primera columna")
+  st.write("Que carajos es una interfaz????")
+  resp = st.checkbox("Preguntele a chatgpt")
+  if resp:
+    st.write("Preste atención a la clase")
