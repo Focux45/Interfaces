10cbdd5c9671c1707b599e56412062090c5e5dc1
Focux45 <134445296+Focux45@users.noreply.github.com>
2024-08-15 07:15:54 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -3,4 +3,5 @@ from PIL import Image
 st.title("😎 MI PRIMERA APP 😎")
 st.header("Aqui subire todo el contenido")
 st.write("Hola mundo")
+image = Image.open("mclaren.jpg")
 st.image(image,captiom = "mclaren")
