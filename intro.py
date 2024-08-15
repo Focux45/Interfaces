4155b59602cd150b1a2a60199b5b3a6040cae248
Focux45 <134445296+Focux45@users.noreply.github.com>
2024-08-15 07:29:10 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -8,12 +8,12 @@ st.image(image,caption = "mclaren")
 texto = st.text_input('Aviso parroquial',"Tienes 20 años y no has conseguido un carro deportivo, pues que lastima yo tampoco")
 st.write("El texto escrito es", texto)
 
-st.subheader("2 Columnas")
+st.subheader("Vamos a probar dos columnas")
 col1, col2 = st.columns(2)
 
 with col1:
   st.subheader("Primera columna")
-  st.write("Que carajos es una interfaz????")
+  st.write("Que es una interfaz?")
   resp = st.checkbox("Preguntele a chatgpt")
   if resp:
     st.write("Preste atención a la clase")
